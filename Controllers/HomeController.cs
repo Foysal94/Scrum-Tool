7dc1dd792e0f18eb74d174085d53ea1e1beae1b8
Foysal94 <Foysal94@outlook.com>
2016-01-04 16:41:03 +0000

    Got the navbar buttons working. There are bugs with the size of it tho. Now going to make a gulp task to watch and sort out sass and coffee files and minify them after

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,21 +12,7 @@ using Microsoft.AspNet.Mvc;
 ***REMOVED***
             return View();
 ***REMOVED***
-
-        public IActionResult About()
-***REMOVED***
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-***REMOVED***
-
-        public IActionResult Contact()
-***REMOVED***
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-***REMOVED***
-
+        
         public IActionResult Error()
 ***REMOVED***
             return View();
--- assets/coffee/Home.coffee
+++ assets/coffee/Home.coffee
@@ -2,5 +2,6 @@
     $('#curved-green-button').on 'click', ->
         $('#InitalButton').animate ***REMOVED***'left': '-1000px'***REMOVED***, 'slow'
  
+ 
  $(document).ready ->
     OnWelcomeButtonClick()

40921495b0f9c7790c5e694498f21308c050614f
Foysal94 <Foysal94@outlook.com>
2016-01-04 12:10:50 +0000

    Got the first button working. Now starting  on the form for the board name to be entered

---
+++ assets/coffee/Home.coffee
@@ -0,0 +1,6 @@
+ OnWelcomeButtonClick = () ->
+    $('#curved-green-button').on 'click', ->
+        $('#InitalButton').animate ***REMOVED***'left': '-1000px'***REMOVED***, 'slow'
+ 
+ $(document).ready ->
+    OnWelcomeButtonClick()
---
+++ wwwroot/js/Home.js
@@ -0,0 +1,13 @@
+var OnWelcomeButtonClick;
+
+OnWelcomeButtonClick = function() ***REMOVED***
+  return $('#curved-green-button').on('click', function() ***REMOVED***
+    return $('#InitalButton').animate(***REMOVED***
+      'left': '-1000px'
+***REMOVED***, 'slow');
+  ***REMOVED***);
+***REMOVED***;
+
+$(document).ready(function() ***REMOVED***
+  return OnWelcomeButtonClick();
+***REMOVED***);

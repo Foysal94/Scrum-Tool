fc8f501d775407fc38c34e58b1a5b95bc863ad90
Foysal94 <Foysal94@outlook.com>
2016-02-18 17:24:54 +0000

    Used bootstrap to split pages into columns and using bootstrap panels as a possible list/column holder thing

---
+++ src/ASPNET5-Scrum-Tool/Models/BoardModel.cs
@@ -0,0 +1,18 @@
+﻿***REMOVED***
+***REMOVED***
+using System.ComponentModel.DataAnnotations;
+***REMOVED***
+***REMOVED***
+using Microsoft.AspNet.Mvc;
+
+***REMOVED***.Models
+***REMOVED***
+    public class BoardModel
+***REMOVED***
+        private string m_BoardName;
+
+        [Required]
+        public string BoardName ***REMOVED*** get ***REMOVED*** return m_BoardName; ***REMOVED*** set ***REMOVED*** m_BoardName = value; ***REMOVED*** ***REMOVED***
+
+***REMOVED***
+***REMOVED***

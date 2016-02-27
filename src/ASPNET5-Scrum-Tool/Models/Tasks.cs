b5fb2bbebb9c842b37b09c9dbe8557d6b09bb727
Foysal <foysal94@outlook.com>
2016-02-27 11:33:21 +0000

    Added inital task model

---
+++ src/ASPNET5-Scrum-Tool/Models/Tasks.cs
@@ -0,0 +1,19 @@
+﻿***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***.Models
+***REMOVED***
+    public class Tasks
+***REMOVED***
+        private ColumnModel m_ParentColumn;
+        private DateTime m_DueDate;
+        private string m_TaskContent;
+
+        public ColumnModel ParentColumn ***REMOVED*** get ***REMOVED***return m_ParentColumn;***REMOVED*** set ***REMOVED*** m_ParentColumn = value***REMOVED*** ***REMOVED***
+        public DateTime DueDate ***REMOVED*** get ***REMOVED*** return DueDate; ***REMOVED***set ***REMOVED*** DueDate = value; ***REMOVED*** ***REMOVED***
+        public string TaskContent ***REMOVED*** get ***REMOVED***return m_TaskContent;***REMOVED*** set ***REMOVED*** m_TaskContent = value; ***REMOVED*** ***REMOVED***
+
+***REMOVED***
+***REMOVED***

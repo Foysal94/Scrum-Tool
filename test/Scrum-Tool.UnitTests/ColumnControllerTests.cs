633efbe34ebc1d9ac769e6c4db63f53a5876adca
Foysal Ahmed <foysal94@outlook.com>
2016-09-04 20:17:43 +0100

    Added method that returns mock/fake column data to be tested on

--- test/Scrum-Tool.UnitTests/ColumnControllerTests.cs
+++ test/Scrum-Tool.UnitTests/ColumnControllerTests.cs
@@ -6,11 +6,37 @@ using Xunit;
 using Moq;
 using Microsoft.AspNetCore.Mvc;
 ***REMOVED***
+using GenFu;
 
 namespace Scrum_Tool.UnitTests
 ***REMOVED***
+    
     public class ColumnControllerTests 
 ***REMOVED***
+        private const int m_BoardID = 0;
+        public ColumnControllerTests() 
+***REMOVED***
+           
+***REMOVED***
+
+        private IQueryable<Columns> GenerateMockData() 
+***REMOVED***
+            int id = 0;
+            int numberOfColumns = 3;
+            string Name = "TestColumn";
+
+            A.Configure<Columns>()
+                .Fill(c => c.ID,
+                     () => ***REMOVED*** return id++; ***REMOVED***)
+                .Fill(c => c.Name, () => ***REMOVED*** return $"***REMOVED***Name***REMOVED******REMOVED***id++***REMOVED***"; ***REMOVED***)
+                .Fill(c => c.BoardID, m_BoardID);
+
+            var columns = A.ListOf<Columns>(numberOfColumns);
+            return columns.AsQueryable();
+***REMOVED***
+
+        
+
 ***REMOVED***
 ***REMOVED***
 

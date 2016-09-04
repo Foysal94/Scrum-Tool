bd0b7272b9ddfc49c7d4998a6f2d1bfb7c2da8ad
Foysal Ahmed <foysal94@outlook.com>
2016-09-05 00:27:02 +0100

    Added the fucntionality to create database using InMemoryTesting. Added test columnController. Moved around the using statements

--- test/Scrum-Tool.UnitTests/ColumnControllerTests.cs
+++ test/Scrum-Tool.UnitTests/ColumnControllerTests.cs
@@ -1,22 +1,42 @@
 ﻿***REMOVED***
+***REMOVED***
+***REMOVED***
+using Microsoft.AspNetCore.Mvc;
+***REMOVED***
 using ASPNET5_Scrum_Tool.Controllers;
 ***REMOVED***
 using FluentAssertions;
 using Xunit;
 using Moq;
-using Microsoft.AspNetCore.Mvc;
-***REMOVED***
 using GenFu;
 
 namespace Scrum_Tool.UnitTests
+***REMOVED***
+
+    public interface ITestRespository<T> 
+***REMOVED***
+        void Add(T model);
+        void Remove(T model);
+        void Edit(T model);
+        void Delete(T model);
+        T FindById(int id);
+        T FindObject(T model);
+        void ReturnCount();
+        IQueryable<T> ReturnAll();
+        void Save();
+
 ***REMOVED***
     
     public class ColumnControllerTests 
 ***REMOVED***
         private const int m_BoardID = 0;
+		  private ScrumToolDB m_ScrumToolDB;
+		  private ColumnController m_ColumnController;
         public ColumnControllerTests() 
 ***REMOVED***
-           
+			  var dbOptions = CreateFakeDatabaseOptions();
+			  m_ScrumToolDB = new ScrumToolDB(dbOptions);
+			  m_ColumnController = new ColumnController(m_ScrumToolDB);
 ***REMOVED***
 
         private IQueryable<Columns> GenerateMockData() 
@@ -35,7 +55,22 @@ namespace Scrum_Tool.UnitTests
             return columns.AsQueryable();
 ***REMOVED***
 
-        
+        private DbContextOptions<ScrumToolDB> CreateFakeDatabaseOptions() 
+		  ***REMOVED***
+			  // Create a fresh service provider, and therefore a fresh 
+            // InMemory database instance.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create a new options instance telling the context to use an
+            // InMemory database and the new service provider.
+            var builder = new DbContextOptionsBuilder<ScrumToolDB>();
+            builder.UseInMemoryDatabase()
+                   .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+		  ***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
